96374a66a3b43c771875d5a028574f1b04a06ffb
juninhofer1 <wilsonfernandes382@gmail.com>
2024-04-26 17:57:02 -0300

    Adicionando projeto ao git

---
+++ src/main/java/br/com/my/church/mychurch/MychurchApplication.java
@@ -0,0 +1,13 @@
+package br.com.my.church.mychurch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MychurchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MychurchApplication.class, args);
+	}
+
+}
